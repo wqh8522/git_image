59f628e487f906f0175387521a423c897e04fc40
wanqh <15180181487@163.com>
2018-11-25 16:33:00 +0800

    add drools demo

---
+++ javaDemo/mydemo-drools/src/main/java/com/wqh/demo/drools/MydemoDroolsApplication.java
@@ -0,0 +1,12 @@
+package com.wqh.demo.drools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydemoDroolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydemoDroolsApplication.class, args);
+	}
+}
