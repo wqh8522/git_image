ed2363e43d156d57991706924791efa2ed84d100
wanqh <15180181487@163.com>
2018-12-02 20:49:31 +0800

    1111

--- javaDemo/mydemo-drools-springboot/src/main/java/com/wqh/springboot/drools/controller/TestDroolsConfControl.java
+++ javaDemo/mydemo-drools-springboot/src/main/java/com/wqh/springboot/drools/controller/TestDroolsConfControl.java
@@ -2,6 +2,7 @@ package com.wqh.springboot.drools.controller;
 
 import com.wqh.springboot.drools.pojo.Product;
 import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.FactHandle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,7 +23,9 @@ public class TestDroolsConfControl {
 
         Product product = new Product();
         product.setType(Product.DIAMOND);
-        kieSession.insert(new String("111"));
+//        kieSession.insert(new String("111"));
+        FactHandle factHandle = kieSession.insert(product);
+        System.out.println(factHandle.toExternalForm());
 
         int i = kieSession.fireAllRules();
         logger.info("Product：{}，触发{}条规则",product,i);
