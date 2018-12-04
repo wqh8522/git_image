4e7c85c48d208b76f4af952b97e0f6ed0d517e6a
wanqh <wanqh@bsfit.com.cn>
2018-12-04 09:25:47 +0800

    drools7 attribute

--- javaDemo/mydemo-drools/src/main/java/com/wqh/demo/drools/service/Drools7TestOne.java
+++ javaDemo/mydemo-drools/src/main/java/com/wqh/demo/drools/service/Drools7TestOne.java
@@ -1,5 +1,6 @@
 package com.wqh.demo.drools.service;
 
+import com.wqh.demo.drools.filter.MyAgendaFilter;
 import com.wqh.demo.drools.pojo.Product;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,6 +9,8 @@ import org.kie.api.KieServices;
 import org.kie.api.definition.KiePackage;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.ActivationGroup;
+import org.kie.api.runtime.rule.AgendaGroup;
 
 public class Drools7TestOne {
 
@@ -21,6 +24,7 @@ public class Drools7TestOne {
         KieContainer kieContainer = kieServices.getKieClasspathContainer();
         //KieSession：一个跟Drools引擎打交道的会话，基于KieBase创建。
         //kmodule.xml :
+        System.setProperty("drools.dateformat", "yyyy-MM-dd");
         kieSession = kieContainer.newKieSession("ksession-rule");
     }
 
@@ -41,15 +45,59 @@ public class Drools7TestOne {
 
 
     @Test
-    public void testNoLoop() throws Exception {
+    public void testTwo() throws Exception {
         Product product = new Product();
         product.setDiscount(100);
-        kieSession.insert(product);
+        //是指定的group获得焦点
+//        kieSession.getAgenda().getAgendaGroup("agenda_group1").setFocus();
+
+//        kieSession.insert(product);
         //触发规则数
         int i = kieSession.fireAllRules();
 //
         System.out.println(i);
 //        System.out.println(product);
+
+        kieSession.dispose();
     }
+
+    @Test
+    public void testFilter() {
+        MyAgendaFilter filter = new MyAgendaFilter("test");
+        kieSession.fireAllRules(filter);
+        kieSession.dispose();
+    }
+
+
+    @Test
+    public void testDateEffective() {
+        KieServices kieServices = KieServices.Factory.get();
+        KieContainer kieContainer = kieServices.getKieClasspathContainer();
+        System.setProperty("drools.dateformat", "yyyy-MM-dd");
+        kieSession = kieContainer.newKieSession("ksession-rule");
+        kieSession.fireAllRules();
+        kieSession.dispose();
+    }
+
+    @Test
+    public void testDurationOrTimer() {
+        new Thread(new Runnable() {
+            public void run() {
+                //保持规则执行,需开启新的线程
+                kieSession.fireUntilHalt();
+            }
+        }).start();
+        try {
+            Thread.sleep(5000);
+            //停止规则的执行
+            kieSession.halt();
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            kieSession.dispose();
+        }
+    }
+
 
 }
