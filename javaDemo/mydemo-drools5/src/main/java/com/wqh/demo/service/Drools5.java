448f037559954087edcc79c41eedba343f802dac
wanqh <15180181487@163.com>
2019-01-13 19:16:22 +0800

    drools5

--- javaDemo/mydemo-drools5/src/main/java/com/wqh/demo/service/Drools5.java
+++ javaDemo/mydemo-drools5/src/main/java/com/wqh/demo/service/Drools5.java
@@ -9,6 +9,7 @@ import org.drools.builder.KnowledgeBuilder;
 import org.drools.builder.KnowledgeBuilderFactory;
 import org.drools.builder.ResourceType;
 import org.drools.definition.KnowledgePackage;
+import org.drools.definition.rule.Rule;
 import org.drools.io.ResourceFactory;
 import org.drools.runtime.StatefulKnowledgeSession;
 import org.drools.runtime.StatelessKnowledgeSession;
@@ -17,6 +18,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
+import java.util.Iterator;
 
 public class Drools5 {
 
@@ -30,7 +32,7 @@ public class Drools5 {
         Person p = new Person();
         p.setFavouriteCheese(c);
         p.setName("person");
-//      KnowledgeBuilder： 在业务代码当中收集已经编译的规则代码，然后对这些规则文件进行编译，最终产生一批编译好的规则包（KnowledgePackage）给其他应用程序使用
+//      KnowledgeBuilder：知识库生成器负责获取文件，如.drl文件、.bpmn2文件或.xls文件；并将其转换为知识库可以使用的规则和流程定义的知识包（KnowledgePackage）
         KnowledgeBuilder knowledgeBuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
 //        编译规则
         knowledgeBuilder.add(ResourceFactory.newClassPathResource("rule/Nodeshared.drl",this.getClass()), ResourceType.DRL);
@@ -40,10 +42,28 @@ public class Drools5 {
         }
 //        产生规则包集合
         Collection<KnowledgePackage> knowledgePackages = knowledgeBuilder.getKnowledgePackages();
+
+//        Iterator<KnowledgePackage> iterator = knowledgePackages.iterator();
+//        while (iterator.hasNext()){
+//            KnowledgePackage next = iterator.next();
+//            logger.info("PackageName>>>>{}",next.getName());
+//            Collection<Rule> rules = next.getRules();
+//            Iterator<Rule> ruleIterator = rules.iterator();
+//            while (ruleIterator.hasNext()){
+//                Rule rule = ruleIterator.next();
+//                logger.info(">>>>>>>>>>>Rule>>>>{}",rule.getName());
+//                logger.info(">>>>>>>>>>>MetaData>>>>{}",rule.getMetaData());
+//                logger.info(">>>>>>>>>>>PackageName>>>>{}",rule.getPackageName());
+//            }
+//            logger.info("FactTypes>>>>{}",next.getFactTypes());
+//            logger.info("FunctionNames>>>>{}",next.getFunctionNames());
+//
+//        }
+
 //      创建KnowledgeBaseConfiguration对象，用来存放规则引擎运行时环境参数的配置对象
-        KnowledgeBaseConfiguration kbConfig = KnowledgeBaseFactory.newKnowledgeBaseConfiguration();
+//        KnowledgeBaseConfiguration kbConfig = KnowledgeBaseFactory.newKnowledgeBaseConfiguration();
 //      KnowledgeBase：收集应用当中知识（knowledge）定义的知识库对象，可以指定一个KnowledgeBaseConfiguration对象：
-        KnowledgeBase knowledgeBase = KnowledgeBaseFactory.newKnowledgeBase(kbConfig);
+        KnowledgeBase knowledgeBase = KnowledgeBaseFactory.newKnowledgeBase();
         //将 KnowledgePackage 集合添加到 KnowledgeBase 当中
         knowledgeBase.addKnowledgePackages(knowledgePackages);
 //      编译完成将规则包文件在引擎当中运行，
